5dd2942a8614676ca34dfce8dd28d9692e54f119
Oleg Chupyra <ovchupyra@gmail.com>
2016-06-11 18:57:31 +0300

    Initial commit

---
+++ src/ru/houm/first/Main.java
@@ -0,0 +1,10 @@
+package ru.houm.first;
+
+/**
+ * Created by Z-Ch on 09.06.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
