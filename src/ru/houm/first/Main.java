f246bfb6a25ae94ecfe5d6ef11de149469eb2abf
Oleg Chupyra <ovchupyra@gmail.com>
2016-06-11 19:45:54 +0300

    add new message

--- src/ru/houm/first/Main.java
+++ src/ru/houm/first/Main.java
@@ -1,10 +1,9 @@
 package ru.houm.first;
 
-/**
- * Created by Z-Ch on 09.06.2016.
- */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("Git and GitHub");
     }
 }
